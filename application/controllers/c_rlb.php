86e3886fc8a79dd641d481270d2a681160a59997
RJunandia <rismawan.junandia@wgs.co.id>
2020-01-08 16:21:34 +0700

    add authorization on RLB Controller

--- application/controllers/c_rlb.php
+++ application/controllers/c_rlb.php
@@ -3,6 +3,15 @@ class C_rlb extends CI_Controller{
 
 	public function __construct(){
 		parent::__construct();
+		if($this->session->userdata('role_id') == NULL){
+			$this->session->set_flashdata('pesan','<div class="alert alert-danger" role="alert">
+			<strong>Anda belum Login !!!</strong>
+			  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+			    <span aria-hidden="true">&times;</span>
+			  </button>
+			</div>');
+			redirect('auth/login');
+		}
 		$this->load->library('Excel');
 		$this->load->model('m_rlb');
 	}
