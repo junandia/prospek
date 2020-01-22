3caed606861a3752a46e884f9d8d000341b58c2c
Nenden Widiyawati <rismawan.junandia@wgs-id.com>
2020-01-22 15:01:37 +0700

    revisi matrix for a,b1,b2

--- application/models/m_rlb.php
+++ application/models/m_rlb.php
@@ -36,5 +36,10 @@
 	                    "'.$pendapatan.'")
 	                ');
 	    } 
+	    public function matrixdet($data){
+			$rumus = ($data[0][0] * $data[1][1] * $data[2][2]) + ($data[0][1] * $data[1][2] * $data[2][0]) + ($data[0][2] * $data[1][0] * $data[2][1]) - ($data[0][1] * $data[1][0] * $data[2][2]) - ($data[0][0] * $data[1][2] * $data[2][1]) - ($data[0][2] * $data[1][1] * $data[2][0]);
+
+			return $rumus;
+		}
 	}
 ?>
